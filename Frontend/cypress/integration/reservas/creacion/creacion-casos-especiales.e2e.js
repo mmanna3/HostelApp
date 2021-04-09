be7d2ccb11a89253f4ec2ed62b076e2a035b6ec9
mmmannna <mmmannna3@gmail.com>
2021-04-08 23:45:38 -0300

    Refactor por eslint

--- Frontend/cypress/integration/reservas/creacion/creacion-casos-especiales.e2e.js
+++ Frontend/cypress/integration/reservas/creacion/creacion-casos-especiales.e2e.js
@@ -1,99 +1,92 @@
-import "cypress-localstorage-commands";
-import * as paginaReservas from "../../../pageObjectModels/reservas/pagina.POM"
+import 'cypress-localstorage-commands';
+import * as paginaReservas from '../../../pageObjectModels/reservas/pagina.POM';
 
-describe('Crear reservas', () => {   
+function mockearSoloUnaHabitacionPrivada() {
+  cy.server();
+  cy.route({
+    method: 'GET',
+    url: '/api/habitaciones/conLugaresLibres**',
+    response: [
+      {
+        id: 1,
+        nombre: 'Roja',
+        esPrivada: true,
+        camas: [
+          {
+            id: 31,
+            nombre: '4',
+            tipo: 'Cucheta Abajo',
+          },
+          {
+            id: 32,
+            nombre: '4',
+            tipo: 'Cucheta Arriba',
+          },
+          {
+            id: 34,
+            nombre: '2',
+            tipo: 'Matrimimonial',
+          },
+          {
+            id: 33,
+            nombre: '1',
+            tipo: 'Individual',
+          },
+        ],
+        cantidadDeLugaresLibres: 5,
+      },
+    ],
+  }).as('conLugaresLibres');
 
-    it('Si la primera es una habitación privada, figura la leyenda correspondiente', () => {
+  cy.visit('/reservas');
+}
 
-      mockearSoloUnaHabitacionPrivada();
+function mockearSoloUnaHabitacionSinCamas() {
+  cy.server();
+  cy.route({
+    method: 'GET',
+    url: '/api/habitaciones/conLugaresLibres**',
+    response: [
+      {
+        id: 1,
+        nombre: 'Roja',
+        esPrivada: true,
+        camas: [],
+        cantidadDeLugaresLibres: 0,
+      },
+    ],
+  }).as('conLugaresLibres');
 
-      paginaReservas.abrirModalCargarNueva();
+  cy.visit('/reservas');
+}
 
-      cy.get('#habitacion-privada-renglon-0')
-         .should('contain.value', 1)
+describe('Crear reservas', () => {
+  it('Si la primera es una habitación privada, figura la leyenda correspondiente', () => {
+    mockearSoloUnaHabitacionPrivada();
 
-      cy.get('#habitacion-privada-renglon-0')
-         .should('contain.text', 'Todas - Habitación privada')
-    })
+    paginaReservas.abrirModalCargarNueva();
 
-    it('Si la primera es una habitación sin camas, figura la leyenda correspondiente', () => {
+    cy.get('#habitacion-privada-renglon-0').should('contain.value', 1);
 
-      mockearSoloUnaHabitacionSinCamas();
-      
-      paginaReservas.abrirModalCargarNueva();
-      
-      cy.get('#habitacion-renglon-0')
-         .should('contain.value', 1)
-
-         cy.get('#renglon-sin-camas-0')
-         .should('contain.value', 'No tiene en esta fecha')
-    })
-})
-
-before(() => {    
-    cy.login();
-    cy.saveLocalStorage();
+    cy.get('#habitacion-privada-renglon-0').should('contain.text', 'Todas - Habitación privada');
   });
-  
-beforeEach(() => {
-   cy.restoreLocalStorage();
+
+  it('Si la primera es una habitación sin camas, figura la leyenda correspondiente', () => {
+    mockearSoloUnaHabitacionSinCamas();
+
+    paginaReservas.abrirModalCargarNueva();
+
+    cy.get('#habitacion-renglon-0').should('contain.value', 1);
+
+    cy.get('#renglon-sin-camas-0').should('contain.value', 'No tiene en esta fecha');
+  });
 });
 
-function mockearSoloUnaHabitacionPrivada(){
-   cy.server()
-   cy.route({
-      method: 'GET',
-      url: '/api/habitaciones/conLugaresLibres**',
-      response: [
-        {
-           "id":1,
-           "nombre":"Roja",
-           "esPrivada":true,
-           "camas":[
-              {
-                 "id":31,
-                 "nombre":"4",
-                 "tipo":"Cucheta Abajo"
-              },
-              {
-                 "id":32,
-                 "nombre":"4",
-                 "tipo":"Cucheta Arriba"
-              },
-              {
-                 "id":34,
-                 "nombre":"2",
-                 "tipo":"Matrimimonial"
-              },
-              {
-                 "id":33,
-                 "nombre":"1",
-                 "tipo":"Individual"
-              }
-           ],
-           "cantidadDeLugaresLibres":5
-        }
-     ]
-    }).as('conLugaresLibres')
-    
-    cy.visit('/reservas')
-}
+before(() => {
+  cy.login();
+  cy.saveLocalStorage();
+});
 
-function mockearSoloUnaHabitacionSinCamas(){
-   cy.server()
-   cy.route({
-      method: 'GET',
-      url: '/api/habitaciones/conLugaresLibres**',
-      response: [
-        {
-           "id":1,
-           "nombre":"Roja",
-           "esPrivada":true,
-           "camas":[],
-           "cantidadDeLugaresLibres":0
-        }
-     ]
-    }).as('conLugaresLibres')
-    
-    cy.visit('/reservas')        
-}
+beforeEach(() => {
+  cy.restoreLocalStorage();
+});
