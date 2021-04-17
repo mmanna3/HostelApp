a383eda159490e91e293b8a1c5e6fab02ad00484
mmmannna <mmmannna3@gmail.com>
2021-04-17 13:10:17 -0300

    Retoque estéticos

--- Frontend/src/pantallas/reservas/Cabecera/Cabecera.tsx
+++ Frontend/src/pantallas/reservas/Cabecera/Cabecera.tsx
@@ -9,6 +9,9 @@ interface IProps {
 
 const Cabecera = ({ showModal }: IProps): ReactElement => {
   const [fechaInicio, modificarFechaInicio] = useState<Date[] | Date>(new Date());
+  const [estiloBotonSemana, modificarEstiloBotonSemana] = useState('');
+  const [estiloBotonDosSemanas, modificarEstiloBotonDosSemanas] = useState('');
+  const [estiloBotonMes, modificarEstiloBotonMes] = useState('');
 
   const invocarApi = (dias: number): void => {
     console.log(dias);
@@ -17,28 +20,41 @@ const Cabecera = ({ showModal }: IProps): ReactElement => {
 
   const seleccionarSemana = (): void => {
     invocarApi(7);
+    modificarEstiloBotonSemana('is-primary is-selected');
+    modificarEstiloBotonDosSemanas('');
+    modificarEstiloBotonMes('');
   };
   const seleccionarDosSemanas = (): void => {
     invocarApi(14);
+    modificarEstiloBotonSemana('');
+    modificarEstiloBotonDosSemanas('is-primary is-selected');
+    modificarEstiloBotonMes('');
   };
   const seleccionarMes = (): void => {
     invocarApi(28);
+    modificarEstiloBotonSemana('');
+    modificarEstiloBotonDosSemanas('');
+    modificarEstiloBotonMes('is-primary is-selected');
   };
 
   return (
     <div className="level container">
       <div className="level-left">
         <div className={`buttons has-addons ${Estilos.botones}`}>
-          <Button onClick={(): void => seleccionarSemana()} text="Semana" className="button is-selected" />
-          <Button onClick={(): void => seleccionarDosSemanas()} text="Dos semanas" className="button" />
-          <Button onClick={(): void => seleccionarMes()} text="Mes" className="button" />
+          <Button onClick={(): void => seleccionarSemana()} text="Semana" className={`button ${estiloBotonSemana}`} />
+          <Button
+            onClick={(): void => seleccionarDosSemanas()}
+            text="Dos semanas"
+            className={`button ${estiloBotonDosSemanas}`}
+          />
+          <Button onClick={(): void => seleccionarMes()} text="Mes" className={`button ${estiloBotonMes}`} />
         </div>
         <div>
           <SelectorDeFecha onChange={modificarFechaInicio} value={fechaInicio} />
         </div>
       </div>
       <div className="level-right">
-        <Button onClick={showModal} text="Cargar nueva" />
+        <Button onClick={showModal} text="Nueva reserva" />
       </div>
     </div>
   );
