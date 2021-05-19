65037ab2817b85d31af371d598aab346d55ae525
mmmannna <mmmannna3@gmail.com>
2021-05-18 21:51:37 -0300

    Agrega validación de nombre para camas cuchetas

--- Backend/Api/Controllers/DTOs/Habitacion/CamaCuchetaDTO.cs
+++ Backend/Api/Controllers/DTOs/Habitacion/CamaCuchetaDTO.cs
@@ -3,8 +3,12 @@
     public class CamaCuchetaDTO
     {
         public int Id { get; set; }
+        
+        [YKNStringLength(Maximo = 10)]
         public string Nombre { get; set; }
+        
         public CamaDTO Abajo { get; set; }
+        
         public CamaDTO Arriba { get; set; }
     }
 }
