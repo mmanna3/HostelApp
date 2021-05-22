1166e7b3817d44bf9a5411cc7163842c51f3282d
mmmannna <mmmannna3@gmail.com>
2021-05-21 22:34:20 -0300

    Crea componente MostrarHabitacionesYCamas

---
+++ Frontend/src/pantallas/reservas/detalle/MostrarHabitacionesYCamas/MostrarHabitacionesYCamas.tsx
@@ -0,0 +1,83 @@
+import { Icon } from 'components/Icon';
+import { obtenerTipoCamaDescripcion } from 'pantallas/reservas/utilidades';
+import React, { ReactElement } from 'react';
+import { CamaDTO, HabitacionDTO } from 'store/api/DTOs';
+import Estilos from './MostrarHabitacionesYCamas.module.scss';
+
+interface IProps {
+  habitacionesPrivadas: HabitacionDTO[];
+  camasDeHabitacionesCompartidas: CamaDTO[];
+}
+
+const MostrarHabitacionesYCamas = ({ habitacionesPrivadas, camasDeHabitacionesCompartidas }: IProps): ReactElement => {
+  const renderizarHabitacionesPrivadas = (habs: HabitacionDTO[]): ReactElement =>
+    habs.length === 0 ? (
+      <div className={Estilos.dato}>
+        <Icon faCode="door-closed" />
+        <p>Habitaciones privadas: Ninguna.</p>
+      </div>
+    ) : (
+      <div className={Estilos.dato}>
+        <Icon faCode="door-closed" />
+        <p>Habitaciones privadas:</p>
+        <ul className={Estilos.lista}>
+          {habs.map(
+            (hab, i): ReactElement => (
+              <li key={i}>{hab.nombre}</li>
+            )
+          )}
+        </ul>
+      </div>
+    );
+
+  const renderizarCamasDeHabitacionesCompartidas = (camas: CamaDTO[]): ReactElement => {
+    if (camas.length === 0)
+      return (
+        <div className={Estilos.dato}>
+          <Icon faCode="bed" />
+          <p>Camas: Ninguna.</p>;
+        </div>
+      );
+
+    const camasAgrupadas: Map<string, CamaDTO[]> = camas.reduce(
+      (entryMap, cama): Map<string, CamaDTO[]> =>
+        entryMap.set(cama.nombreHabitacion, [...(entryMap.get(cama.nombreHabitacion) || []), cama]),
+      new Map()
+    );
+
+    let camasLi: ReactElement[] = [];
+    camasAgrupadas.forEach((camas, habitacionNombre): void => {
+      camasLi.push(
+        <li key={habitacionNombre}>
+          Habitación {habitacionNombre}
+          <ul className={Estilos.listaCamasDeHabitacionesCompartidas}>
+            {camas.map(
+              (c, i): ReactElement => (
+                <li key={i}>
+                  Cama {c.nombre} ({obtenerTipoCamaDescripcion.get(c.tipo)})
+                </li>
+              )
+            )}
+          </ul>
+        </li>
+      );
+    });
+
+    return (
+      <div className={Estilos.dato}>
+        <Icon faCode="bed" />
+        <p>Camas de habitaciones compartidas: </p>
+        <ul className={Estilos.lista}>{camasLi}</ul>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      {renderizarCamasDeHabitacionesCompartidas(camasDeHabitacionesCompartidas)}
+      {renderizarHabitacionesPrivadas(habitacionesPrivadas)}
+    </>
+  );
+};
+
+export default MostrarHabitacionesYCamas;
