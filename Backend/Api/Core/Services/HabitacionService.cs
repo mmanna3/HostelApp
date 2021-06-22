c8fa9e6f0461eeb131064fce528279f70a486b5e
mmmannna <mmmannna3@gmail.com>
2021-06-22 00:31:31 -0300

    No permite crear dos habitaciones con el mismo nombre

--- Backend/Api/Core/Services/HabitacionService.cs
+++ Backend/Api/Core/Services/HabitacionService.cs
@@ -36,66 +36,70 @@ namespace Api.Core.Services
             return await _habitacionRepository.ObtenerPorId(id);
         }
 
-        public async Task<int> CrearAsync(Habitacion Habitacion)
+        public async Task<int> CrearAsync(Habitacion habitacion)
         {
-            if (HayCamasSinNombre(Habitacion))
+            if (HayCamasSinNombre(habitacion))
                 throw new AppException("Todas las camas deben tener Identificador");
 
-            if (HayCamasConIdentificadorRepetido(Habitacion))
+            if (HayCamasConIdentificadorRepetido(habitacion))
                 throw new AppException("No puede haber camas con el mismo Identificador");
 
-            _habitacionRepository.Crear(Habitacion);
+            var habitaciones = await _habitacionRepository.Listar();
+            if (habitaciones.Any(x => x.Nombre.ToLower() == habitacion.Nombre.ToLower()))
+                throw new AppException("Ya existe una habitación con ese nombre");
+
+            _habitacionRepository.Crear(habitacion);
             await _unitOfWork.CompleteAsync();
-            return Habitacion.Id;
+            return habitacion.Id;
         }
 
-        public async Task ModificarAsync(int id, Habitacion Habitacion)
+        public async Task ModificarAsync(int id, Habitacion habitacion)
         {
             var habitacionAModificar = await _habitacionRepository.ObtenerPorId(id);
 
             if (habitacionAModificar == null)
                 throw new AppException($"No se encontró la habitación de id:{id}");
 
-            Habitacion.Id = habitacionAModificar.Id;
-            _habitacionRepository.Modificar(habitacionAModificar, Habitacion);
+            habitacion.Id = habitacionAModificar.Id;
+            _habitacionRepository.Modificar(habitacionAModificar, habitacion);
             
             await _unitOfWork.CompleteAsync();
         }
 
-        private static bool HayCamasSinNombre(Habitacion Habitacion)
+        private static bool HayCamasSinNombre(Habitacion habitacion)
         {
-            return Habitacion.CamasIndividuales != null && 
-                   Habitacion.CamasIndividuales.ToList().Exists(x => string.IsNullOrEmpty(x.Nombre))
+            return habitacion.CamasIndividuales != null && 
+                   habitacion.CamasIndividuales.ToList().Exists(x => string.IsNullOrEmpty(x.Nombre))
                    ||
-                   Habitacion.CamasMatrimoniales != null && 
-                   Habitacion.CamasMatrimoniales.ToList().Exists(x => string.IsNullOrEmpty(x.Nombre))
+                   habitacion.CamasMatrimoniales != null && 
+                   habitacion.CamasMatrimoniales.ToList().Exists(x => string.IsNullOrEmpty(x.Nombre))
                    ||
-                   Habitacion.CamasCuchetas != null && 
-                   Habitacion.CamasCuchetas
+                   habitacion.CamasCuchetas != null && 
+                   habitacion.CamasCuchetas
                        .Select(x => x.Abajo)
                        .ToList()
                        .Exists(x => string.IsNullOrEmpty(x.Nombre))
                    ||
-                   Habitacion.CamasCuchetas != null &&
-                   Habitacion.CamasCuchetas
+                   habitacion.CamasCuchetas != null &&
+                   habitacion.CamasCuchetas
                        .Select(x => x.Arriba)
                        .ToList()
                        .Exists(x => string.IsNullOrEmpty(x.Nombre))
                 ;
         }
 
-        private static bool HayCamasConIdentificadorRepetido(Habitacion Habitacion)
+        private static bool HayCamasConIdentificadorRepetido(Habitacion habitacion)
         {
             var nombres = new List<string>();
 
-            if (Habitacion.CamasMatrimoniales != null)
-                nombres.AddRange(Habitacion.CamasMatrimoniales?.Select(x => x.Nombre));
+            if (habitacion.CamasMatrimoniales != null)
+                nombres.AddRange(habitacion.CamasMatrimoniales?.Select(x => x.Nombre));
 
-            if (Habitacion.CamasCuchetas != null)
-                nombres.AddRange(Habitacion.CamasCuchetas.Select(x => x.Abajo.Nombre)); //Es el mismo nombre arriba y abajo
+            if (habitacion.CamasCuchetas != null)
+                nombres.AddRange(habitacion.CamasCuchetas.Select(x => x.Abajo.Nombre)); //Es el mismo nombre arriba y abajo
 
-            if (Habitacion.CamasIndividuales != null)
-                nombres.AddRange(Habitacion.CamasIndividuales.Select(x => x.Nombre));
+            if (habitacion.CamasIndividuales != null)
+                nombres.AddRange(habitacion.CamasIndividuales.Select(x => x.Nombre));
 
             return nombres.GroupBy(x => x).Any(g => g.Count() > 1);
         }
