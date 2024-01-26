1517e17615aea308d30a2ecd26c50efbf4f3891f
mmanna3 <mmmannna3@gmail.com>
2024-01-25 21:48:23 -0300

    FIX: Al hacerle click a la X del calendario, se rompía todo

--- Frontend/src/components/dateRangePicker/DateRangePicker.js
+++ Frontend/src/components/dateRangePicker/DateRangePicker.js
@@ -2,7 +2,7 @@ import Drp from '@wojtekmaj/react-daterange-picker';
 import { Input } from 'components/Input';
 import React from 'react';
 import { useFormContext } from 'react-hook-form';
-import { convertirAString } from 'utils/Fecha';
+import { convertirAString, hoy, maniana } from 'utils/Fecha';
 import Styles from './DateRangePicker.module.scss';
 
 // interface IProps {
@@ -15,6 +15,10 @@ import Styles from './DateRangePicker.module.scss';
 
 const DateRangePicker = ({ valor, etiqueta, actualizarValor, desdeName, hastaName }) => {
   const { setValue } = useFormContext();
+
+  if (!valor) {
+    valor = [hoy(), maniana()]
+  }
 
   return (
     <div className="field">
